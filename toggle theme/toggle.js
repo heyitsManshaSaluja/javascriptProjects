f7f6bf3b6a0e62e96e8aea2032496186c6f68781
heyitsManshaSaluja <manshasaluja4@gmail.com>
2025-04-05 17:42:15 +0530

    Delete toggle theme directory

--- toggle theme/toggle.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById("themetoggle");
-
-toggleButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    document.body.classList.toggle("light-mode");
-});
